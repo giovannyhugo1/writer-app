c816b79164bb93183d6495598b5ac94f497efd63
giovannyhugo1 <giovannyhugo1@gmail.com>
2026-08-29 17:38:36 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'writer-pwa-v1';
+const urlsToCache = [
+    './writer.html',
+    './manifest.json'
+];
+
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function(cache) {
+                console.log('缓存静态资源');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function(response) {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
